f6f284f6c5f098174aed5492cc0d0483b4fb57a4
Xavier Roumegue <xroumegue@gmail.com>
2022-03-29 14:30:12 +0200

    expand the tabs
    Signed-off-by: Xavier Roumegue <xroumegue@gmail.com>

--- FIT.sh
+++ FIT.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2154
 
 if [ -z ${fit_conf_prefix+x} ] ; then
-	fit_conf_prefix="conf-"
+    fit_conf_prefix="conf-"
 fi
 
 get_dtb_feature() {
@@ -32,122 +32,122 @@ get_fdt_name() {
 }
 
 make_FIT_image() {
-	local kernel_compression
-	local kernel_its=${1:-$output_dir/arch/$arch/boot/kernel_fdt.its}
+    local kernel_compression
+    local kernel_its=${1:-$output_dir/arch/$arch/boot/kernel_fdt.its}
 
-	case $image_kernel in
-	*.gz)
-		kernel_compression=gzip
-		;;
-	*.lz4)
-		kernel_compression=lz4
-		;;
-	*)
-		kernel_compression=none
-		;;
-	esac
+    case $image_kernel in
+    *.gz)
+        kernel_compression=gzip
+        ;;
+    *.lz4)
+        kernel_compression=lz4
+        ;;
+    *)
+        kernel_compression=none
+        ;;
+    esac
 
-	cat <<EOF > "$kernel_its"
+    cat <<EOF > "$kernel_its"
 /dts-v1/;
 / {
-	description = "Kernel + FDT image for $platform board";
-	#address-cells = <1>;
+    description = "Kernel + FDT image for $platform board";
+    #address-cells = <1>;
 
-	images {
-		kernel {
-			description = "Linux kernel";
-			data = /incbin/("$output_dir/arch/$arch/boot/$image_kernel");
-			type = "kernel";
-			arch = "$arch";
-			os = "linux";
-			compression = "$kernel_compression";
-			load = <$loadaddr>;
-			entry = <$loadaddr>;
-			hash-1 {
-				algo = "crc32";
-			};
-			hash-2 {
-				algo = "sha1";
-			};
-		};
+    images {
+        kernel {
+            description = "Linux kernel";
+            data = /incbin/("$output_dir/arch/$arch/boot/$image_kernel");
+            type = "kernel";
+            arch = "$arch";
+            os = "linux";
+            compression = "$kernel_compression";
+            load = <$loadaddr>;
+            entry = <$loadaddr>;
+            hash-1 {
+                algo = "crc32";
+            };
+            hash-2 {
+                algo = "sha1";
+            };
+        };
 EOF
 
-	local dtb
-	local addr=$fdtaddr
+    local dtb
+    local addr=$fdtaddr
 
-	for dtb in "${dtbs[@]}" "${dt_overlays[@]}" ; do
-		local src=${dtb/:*/}
-		local dst=${dtb/*:/}
-		local load=
+    for dtb in "${dtbs[@]}" "${dt_overlays[@]}" ; do
+        local src=${dtb/:*/}
+        local dst=${dtb/*:/}
+        local load=
 
-		if [ -n "$fdtaddr" ] ; then
-			load="load = <$(printf 0x%08x "$addr")>;"
-			addr=$((addr+0x40000))
-		fi
+        if [ -n "$fdtaddr" ] ; then
+            load="load = <$(printf 0x%08x "$addr")>;"
+            addr=$((addr+0x40000))
+        fi
 
-		if [[ ${src} != /* ]] ; then
-			src="$output_dir/arch/$arch/boot/dts/$src"
-		fi
+        if [[ ${src} != /* ]] ; then
+            src="$output_dir/arch/$arch/boot/dts/$src"
+        fi
 
         dst=$(get_fdt_name "${dst}")
 
-		cat <<EOF >> "$kernel_its"
-		fdt-$dst {
-			description = "Flattened Device Tree blob $dst";
-			data = /incbin/("$src");
-			type = "flat_dt";
-			arch = "$arch";
-			compression = "none";
-			$load
-			hash-1 {
-				algo = "crc32";
-			};
-			hash-2 {
-				algo = "sha1";
-			};
-		};
+        cat <<EOF >> "$kernel_its"
+        fdt-$dst {
+            description = "Flattened Device Tree blob $dst";
+            data = /incbin/("$src");
+            type = "flat_dt";
+            arch = "$arch";
+            compression = "none";
+            $load
+            hash-1 {
+                algo = "crc32";
+            };
+            hash-2 {
+                algo = "sha1";
+            };
+        };
 EOF
-	done
+    done
 
-	local config=$platform
-	cat <<EOF >> "$kernel_its"
-	};
+    local config=$platform
+    cat <<EOF >> "$kernel_its"
+    };
 
-	configurations {
-		default = "${fit_conf_prefix}${config}";
+    configurations {
+        default = "${fit_conf_prefix}${config}";
 EOF
-	for dtb in "${dtbs[@]}" ; do
-		local dst=${dtb/*:/}
-		dst=${dst/*\//}
+    for dtb in "${dtbs[@]}" ; do
+        local dst=${dtb/*:/}
+        dst=${dst/*\//}
         dst=$(get_fdt_name "${dst}")
 
-		cat <<EOF >> "$kernel_its"
-		${fit_conf_prefix}${dst} {
-			description = "Boot Linux kernel with $dst blob";
-			kernel = "kernel";
-			fdt = "fdt-$dst";
-		};
+        cat <<EOF >> "$kernel_its"
+        ${fit_conf_prefix}${dst} {
+            description = "Boot Linux kernel with $dst blob";
+            kernel = "kernel";
+            fdt = "fdt-$dst";
+        };
 EOF
-	done
+    done
 
-	for dtbo in "${dt_overlays[@]}" ; do
-		local dst=${dtbo/*:/}
+    for dtbo in "${dt_overlays[@]}" ; do
+        local dst=${dtbo/*:/}
         dst=$(get_fdt_name "${dst}")
 
-		cat <<EOF >> "$kernel_its"
-		${fit_conf_prefix}${dst} {
-			description = "DT overlay $dst";
-			fdt = "fdt-$dst";
-		};
+        cat <<EOF >> "$kernel_its"
+        ${fit_conf_prefix}${dst} {
+            description = "DT overlay $dst";
+            fdt = "fdt-$dst";
+        };
 EOF
-	done
+    done
 
-	cat <<EOF >> "$kernel_its"
-	};
+    cat <<EOF >> "$kernel_its"
+    };
 };
 EOF
 
-	PATH="$output_dir/scripts/dtc:$PATH" mkimage -f "$kernel_its" "${kernel_its/its/itb}"
+    PATH="$output_dir/scripts/dtc:$PATH" mkimage -f "$kernel_its" "${kernel_its/its/itb}"
 }
 
 
