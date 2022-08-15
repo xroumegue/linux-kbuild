945ca799b6697e55281b841344cef81397f290e6
Xavier Roumegue <xroumegue@gmail.com>
2022-08-15 19:54:45 +0200

    FIT: Rework configuration name
    Signed-off-by: Xavier Roumegue <xroumegue@gmail.com>

--- FIT.sh
+++ FIT.sh
@@ -1,8 +1,35 @@
 # SPDX-License-Identifier: GPL-2.0+
+# shellcheck disable=SC2154
 
 if [ -z ${fit_conf_prefix+x} ] ; then
 	fit_conf_prefix="conf-"
 fi
+
+get_dtb_feature() {
+    local dtb_feature
+    dtb_feature=$(basename "${1}" "$2")
+    if [ -n "${dtb_feature/$platform-/}" ]; then
+        dtb_feature=${dtb_feature/$platform-/}
+    fi
+    echo "${dtb_feature}"
+}
+
+get_fdt_name() {
+    local fdt_name
+    fdt_name=$1
+    case "${fdt_name##*.}" in
+        "dtb")
+            fdt_name=$(basename "${fdt_name}" .dtb)
+            ;;
+        "dtbo")
+            fdt_name=$(get_dtb_feature "${fdt_name}" .dtbo)
+            ;;
+        *)
+            fatal "Invalid dtb file ${fdt_name} ${fdt_name##*.} "
+            ;;
+    esac
+    echo "${fdt_name}"
+}
 
 make_FIT_image() {
 	local kernel_compression
@@ -20,7 +47,7 @@ make_FIT_image() {
 		;;
 	esac
 
-	cat <<EOF > $kernel_its
+	cat <<EOF > "$kernel_its"
 /dts-v1/;
 / {
 	description = "Kernel + FDT image for $platform board";
@@ -48,22 +75,23 @@ EOF
 	local dtb
 	local addr=$fdtaddr
 
-	for dtb in ${dtbs[@]} ${dt_overlays[@]} ; do
+	for dtb in "${dtbs[@]}" "${dt_overlays[@]}" ; do
 		local src=${dtb/:*/}
 		local dst=${dtb/*:/}
 		local load=
 
-		if [ ! -z $fdtaddr ] ; then
-			load="load = <$(printf 0x%08x $addr)>;"
-			addr=$(($addr+0x40000))
+		if [ -n "$fdtaddr" ] ; then
+			load="load = <$(printf 0x%08x "$addr")>;"
+			addr=$((addr+0x40000))
 		fi
 
 		if [[ ${src} != /* ]] ; then
 			src="$output_dir/arch/$arch/boot/dts/$src"
 		fi
 
-		dst=${dst/*\//}
-		cat <<EOF >> $kernel_its
+        dst=$(get_fdt_name "${dst}")
+
+		cat <<EOF >> "$kernel_its"
 		fdt-$dst {
 			description = "Flattened Device Tree blob $dst";
 			data = /incbin/("$src");
@@ -81,18 +109,19 @@ EOF
 EOF
 	done
 
-	local config=$(echo ${dtbs[0]} | sed 's/.*[:/]//')
-	cat <<EOF >> $kernel_its
+	local config=$platform
+	cat <<EOF >> "$kernel_its"
 	};
 
 	configurations {
 		default = "${fit_conf_prefix}${config}";
 EOF
-	for dtb in ${dtbs[@]} ; do
+	for dtb in "${dtbs[@]}" ; do
 		local dst=${dtb/*:/}
 		dst=${dst/*\//}
+        dst=$(get_fdt_name "${dst}")
 
-		cat <<EOF >> $kernel_its
+		cat <<EOF >> "$kernel_its"
 		${fit_conf_prefix}${dst} {
 			description = "Boot Linux kernel with $dst blob";
 			kernel = "kernel";
@@ -101,11 +130,11 @@ EOF
 EOF
 	done
 
-	for dtbo in ${dt_overlays[@]} ; do
+	for dtbo in "${dt_overlays[@]}" ; do
 		local dst=${dtbo/*:/}
-		dst=${dst/*\//}
+        dst=$(get_fdt_name "${dst}")
 
-		cat <<EOF >> $kernel_its
+		cat <<EOF >> "$kernel_its"
 		${fit_conf_prefix}${dst} {
 			description = "DT overlay $dst";
 			fdt = "fdt-$dst";
@@ -113,12 +142,11 @@ EOF
 EOF
 	done
 
-	cat <<EOF >> $kernel_its
+	cat <<EOF >> "$kernel_its"
 	};
 };
 EOF
 
-	PATH="$output_dir/scripts/dtc:$PATH" mkimage -f $kernel_its ${kernel_its/its/itb}
+	PATH="$output_dir/scripts/dtc:$PATH" mkimage -f "$kernel_its" "${kernel_its/its/itb}"
 }
 
-_image_file=kernel_fdt.itb
